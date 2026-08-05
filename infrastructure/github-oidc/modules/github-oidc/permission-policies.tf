380693c162a258bc84a4b49592f1cec47434a6df
meeps-dev <therealmeepor@gmail.com>
2026-08-05 02:37:28 +0100

    ci: add Terraform format validate and OIDC plan workflow

--- infrastructure/github-oidc/modules/github-oidc/permission-policies.tf
+++ infrastructure/github-oidc/modules/github-oidc/permission-policies.tf
@@ -174,6 +174,7 @@ data "aws_iam_policy_document" "terraform_read" {
       effect = "Allow"
 
       actions = [
+        "s3:GetAccelerateConfiguration",
         "s3:GetBucket*",
         "s3:GetEncryptionConfiguration",
         "s3:GetLifecycleConfiguration",
