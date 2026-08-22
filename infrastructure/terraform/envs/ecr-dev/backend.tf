d9a863568885c8a712af3ba3e91f380256937a96
meeps-dev <therealmeepor@gmail.com>
2026-08-22 18:01:58 +0100

    migrated ecr backend

--- infrastructure/terraform/envs/ecr-dev/backend.tf
+++ infrastructure/terraform/envs/ecr-dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "meeps-terraform-state-256748318717-eu-west-2"
-    key          = "week-12/ecr-dev/terraform.tfstate"
+    key          = "ecr-repository/ecr-dev/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
     use_lockfile = true
